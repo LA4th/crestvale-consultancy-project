2c263b3372893c0d43965e9c38a9970bf73c7436
Enzo Laforteza <lorenzolaforteza311@gmail.com>
2025-10-03 14:28:52 +0800

    Adding the vision goal page no code in file

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
   <div class="max-w-full w-[900px] m-auto px-2 md:px-0">
     <TheHeader></TheHeader>
     <TheLandingPage></TheLandingPage>
+    <TheVisionGoalPage></TheVisionGoalPage>
   </div>
 </template>
 <script setup>
 import TheHeader from './Components/TheHeader.vue';
 import TheLandingPage from './Components/TheLandingPage.vue';
-
-
+import TheVisionGoalPage from './Components/TheVisionGoalPage.vue';
 </script>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import './style.css'
 import App from './App.vue'
 import TheHeader from './Components/TheHeader.vue';
 import TheLandingPage from './Components/TheLandingPage.vue';
+import TheVisionGoalPage from './Components/TheVisionGoalPage.vue';
 import ButtonCall from './Components/ButtonCall.vue';
 
 const app = createApp(App);
 // REGISTER GLOBAL
 app.component('TheHeader', TheHeader);
 app.component('TheLandingPage', TheLandingPage);
+app.component('TheVisionGoalPage', TheVisionGoalPage);
 app.component('ButtonCall', ButtonCall);
 
 app.mount('#app')
