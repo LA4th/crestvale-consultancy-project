a0e6b400d5560f99347b137e2493e7a93cd02ed7
LA4th <lorenzolaforteza311@gmail.com>
2025-10-04 07:51:32 +0800

    Adding the vision and goal page

--- src/Components/TheVisionGoalPage.vue
+++ src/Components/TheVisionGoalPage.vue
@@ -1,6 +1,45 @@
 <template>
-  <div>
-    
+  <div class="mt-5">
+    <!-- VISION AND GOAL -->
+    <div class="flex flex-col gap-y-5 md:flex-row md:gap-x-5 animate-fade-in-end animate-once animate-duration-400 animate-delay-1 animate-normal">
+      <!-- CARD 1 -->
+      <div class="bg-gray-white max-w-full w-[358px] h-[251px] px-3 py-4 text-wrap rounded-xl flex flex-col gap-y-2 md:h-full">
+        <!-- HEADER CARD 1 -->
+        <div class="flex flex-row items-center gap-x-2">
+          <span class="w-14 h-14 flex items-center justify-center rounded-full bg-dark-blue">
+            <i class="fa-solid fa-eye text-white text-4xl"></i>
+          </span>
+          <span class="font-roboto text-[20px] tracking-tight text-slight-black font-bold">In vision your goals</span>
+        </div>
+        <p class="text-gray indent-8">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae qui, quos at corporis voluptate aliquid unde aspernatur repellendus nostrum, recusandae sequi officia, provident quisquam. Odio quibusdam quas repellendus magnam totam.</p> 
+      </div>
+      <!-- CARD 2 -->
+      <div class="bg-gray-white max-w-full w-[358px] h-[251px] px-3 py-4 text-wrap rounded-xl flex flex-col gap-y-2 md:h-full">
+        <!-- HEADER CARD 2 -->
+        <div class="flex flex-row items-center gap-x-2">
+          <span class="w-14 h-14 flex items-center justify-center rounded-full bg-dark-blue">
+            <i class="fa-solid fa-rocket text-white text-4xl"></i>
+          </span>
+          <span class="font-roboto text-[20px] tracking-tight text-slight-black font-bold">Launch your business</span>
+        </div>
+        <p class="text-gray indent-8">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae qui, quos at corporis voluptate aliquid unde aspernatur repellendus nostrum, recusandae sequi officia, provident quisquam. Odio quibusdam quas repellendus magnam totam.</p> 
+      </div>
+      <!-- CARD 3 -->
+      <div class="bg-gray-white max-w-full w-[358px] h-[251px] px-3 py-4 text-wrap rounded-xl flex flex-col gap-y-2 md:h-full">
+        <!-- HEADER CARD 3 -->
+        <div class="flex flex-row items-center gap-x-2">
+          <span class="w-14 h-14 flex items-center justify-center rounded-full bg-dark-blue">
+            <i class="fa-solid fa-trophy text-white text-4xl"></i>
+          </span>
+          <span class="font-roboto text-[20px] tracking-tight text-slight-black font-bold">Become a leader</span>
+        </div>
+        <p class="text-gray indent-8">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae qui, quos at corporis voluptate aliquid unde aspernatur repellendus nostrum, recusandae sequi officia, provident quisquam. Odio quibusdam quas repellendus magnam totam.</p>
+      </div>
+    </div>
+    <!-- APPLY SECTION -->
+    <div>
+      asdasdsa
+    </div>
   </div>
 </template>
 <script setup>
